058a8bc82959f6f725f4dc422a4644c88e5ca0e9
Ubuntu <ubuntu@ip-172-31-47-145.eu-west-1.compute.internal>
2021-07-29 12:48:54 +0000

    testing teams notification on push, deploy / test are still failing

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -9,4 +9,4 @@ source venv/bin/activate
 python3 -m pytest --cov=application --junitxml=junit/test-results.xml --cov-report=xml --cov-report=term-missing
 
 # testing webhook - it works!
-# testing teams notification
+# still testing teams notification
