60436737fd1900e49961b0c397ed5f4038ddec08
Ubuntu <ubuntu@ip-172-31-47-145.eu-west-1.compute.internal>
2021-07-29 12:52:46 +0000

    teams notifications aint working, went back to working on the apparent create.py issue

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -9,7 +9,7 @@ sudo apt-get install python3 python3-pip python3-venv -y
 
 # Create and source virtual environment
 python3 -m venv venv
-python3 create.py
+
 source venv/bin/activate
 
 
--- jenkins/install.sh
+++ jenkins/install.sh
@@ -8,6 +8,7 @@ sudo apt-get install -y python3 python3-pip python3-venv
 # Create and activate the virtual environment:
 
 python3 -m venv venv
+python3 create.py
 source venv/bin/activate
 
 # Install pip requirements:
