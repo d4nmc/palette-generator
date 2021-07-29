fa6d7304266275000ac9f46f86373fe8fb8b10c6
Ubuntu <ubuntu@ip-172-31-47-145.eu-west-1.compute.internal>
2021-07-29 12:37:11 +0000

    testing webhook, which works but deploy / test are still failing

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -8,4 +8,4 @@ source venv/bin/activate
 
 python3 -m pytest --cov=application --junitxml=junit/test-results.xml --cov-report=xml --cov-report=term-missing
 
-# testing webhook
+# testing webhook - it works!
