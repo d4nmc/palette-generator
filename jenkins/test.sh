c95979a1d37aff52b9bbfff94bc7e4ca808419b3
Ubuntu <ubuntu@ip-172-31-47-145.eu-west-1.compute.internal>
2021-07-30 14:16:14 +0000

    MC:coverage reports edited

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -5,5 +5,5 @@ source venv/bin/activate
 python3 -m pytest --cov=application --junitxml=junit/test-results.xml --cov-report=xml --cov-report=term-missing
 
 
-# further testing - 2
+# further testing - 3
 
