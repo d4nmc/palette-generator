6d65a280a66d6b345a8191b662de4fdd05fc51e1
Ubuntu <ubuntu@ip-172-31-47-145.eu-west-1.compute.internal>
2021-07-30 13:28:56 +0000

    MC:playing with the cloning line and position to test

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Virtual environment needs reinstalled before the tests will run:
-
 source venv/bin/activate
-
-# To generate JUnit and Cobertura coverage reports:
 
 python3 -m pytest --cov=application --junitxml=junit/test-results.xml --cov-report=xml --cov-report=term-missing
 
-# testing webhook - it works!
-# still testing teams notification - it works!
 
